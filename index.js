40f06663a392085279e5949c98ed05ba15a3621e
Vitalii Salai <vitalii.v.salai@gmail.com>
2021-01-18 00:02:44 +0200

    quotes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('@koa/cors')
 const Router = require('koa-router')
 const BodyParser = require('koa-bodyparser')
 const logger = require('koa-logger')
-const ObjectID = require("mongodb").ObjectID
+const ObjectID = require('mongodb').ObjectID
 
 const app = new Koa()
 const router = new Router()
@@ -13,32 +13,32 @@ app.use(logger())
 app.use(BodyParser())
 require('./mongo')(app)
 
-router.get("/tasks", async (ctx) => {
+router.get('/tasks', async (ctx) => {
     ctx.status = 200
     ctx.body = await ctx.app.tasks.find().toArray()
 })
 
-router.post("/tasks", async (ctx) => {
+router.post('/tasks', async (ctx) => {
     await ctx.app.tasks.insertOne(ctx.request.body)
     ctx.status = 201
     ctx.body = await ctx.app.tasks.find().toArray()
 })
 
-router.get("/tasks/:id", async (ctx) => {
+router.get('/tasks/:id', async (ctx) => {
     ctx.status = 200
-    ctx.body = await ctx.app.tasks.findOne({ "_id": ObjectID(ctx.params.id) })
+    ctx.body = await ctx.app.tasks.findOne({ '_id': ObjectID(ctx.params.id) })
 })
 
-router.put("/tasks/:id", async (ctx) => {
-    const documentQuery = { "_id": ObjectID(ctx.params.id) }
+router.put('/tasks/:id', async (ctx) => {
+    const documentQuery = { '_id': ObjectID(ctx.params.id) }
     const valuesToUpdate = { $set: ctx.request.body }
     await ctx.app.tasks.updateOne(documentQuery, valuesToUpdate)
     ctx.status = 200
     ctx.body = await ctx.app.tasks.find().toArray()
 })
 
-router.delete("/tasks/:id", async (ctx) => {
-    const documentQuery = { "_id": ObjectID(ctx.params.id) }
+router.delete('/tasks/:id', async (ctx) => {
+    const documentQuery = { '_id': ObjectID(ctx.params.id) }
     await ctx.app.tasks.deleteOne(documentQuery)
     ctx.status = 200
     ctx.body = await ctx.app.tasks.find().toArray()
