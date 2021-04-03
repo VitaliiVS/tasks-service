2f0172ba419552142709a853de5fe37162140277
Vitalii Salai <vitalii.v.salai@gmail.com>
2021-04-03 17:46:15 +0300

    DELETE refactor

--- tasks.js
+++ tasks.js
@@ -77,15 +77,13 @@ router.get('/:id', async (ctx) => {
 
 router.delete('/:id', async (ctx) => {
     const documentQuery = { '_id': ObjectID(ctx.params.id) }
-
     const headers = ctx.request.header
     const jwt = jsonwebtoken.decode(headers.authorization.slice(7))
     const user = jwt.payload.user
-
     const task = await ctx.app.tasks.find({ '_id': ObjectID(ctx.params.id) }).toArray()
 
     if (user.userId === task[0].createdBy) {
-        await ctx.app.tasks.deleteOne(documentQuery)
+        await ctx.app.tasks.updateOne(documentQuery, { $set: { 'isDeleted': true } })
         ctx.status = 200
         ctx.body = await ctx.app.tasks.find({ 'createdBy': user.userId, 'isDeleted': false }).toArray()
     } else {
