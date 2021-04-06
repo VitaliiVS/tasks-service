51f6294865bb2b529df4a39780b2636ed7fdfe15
Vitalii Salai <vitalii.v.salai@gmail.com>
2021-04-06 19:49:58 +0300

    POST validation for empty title

--- tasks.js
+++ tasks.js
@@ -17,7 +17,13 @@ router.get('/', async (ctx) => {
 })
 
 router.post('/', async (ctx) => {
-    if (Object.keys(ctx.request.body).length !== 0) {
+    if (Object.keys(ctx.request.body).length === 0) {
+        ctx.status = 400
+        ctx.body = { error: 'Request body can\'t be empty' }
+    } else if (ctx.request.body.taskLabel.trim().length === 0) {
+        ctx.status = 400
+        ctx.body = { error: 'Task title can\'t be empty' }
+    } else {
         const headers = ctx.request.header
         const jwt = jsonwebtoken.decode(headers.authorization.slice(7))
         const user = jwt.payload.user
@@ -28,9 +34,6 @@ router.post('/', async (ctx) => {
         await ctx.app.tasks.insertOne(task)
         ctx.status = 201
         ctx.body = await ctx.app.tasks.find({ 'createdBy': user.userId, 'isDeleted': false }).toArray()
-    } else {
-        ctx.status = 400
-        ctx.body = { error: 'Body can\'t be empty' }
     }
 })
 
