e58632b75c3745db876039c4cb97a4ed503ce3e4
Vitalii Salai <vitalii.v.salai@gmail.com>
2021-04-14 18:32:46 +0300

    uri

--- src/mongo.ts
+++ src/mongo.ts
@@ -1,9 +1,8 @@
 import { MongoClient } from 'mongodb'
 import * as assert from 'assert'
 
-// const mongoUri = 'mongodb://localhost:27017/'
-const mongoUri =
-	'mongodb+srv://Braindamage93:Braindamage93@cluster0.krsnf.mongodb.net/db?retryWrites=true&w=majority'
+const mongoUri = 'mongodb://localhost:27017/'
+
 const dbName = 'db'
 const tasks = 'tasks'
 const people = 'people'
